3d5256f7dccc5f5396ad8e8c6448e65c5e4341e1
ayecespedes <ayecespedes3001@gmail.com>
2024-05-06 10:49:35 -0300

    se creó la estructura Dia Semana

---
+++ assets/js/diaSemana.js
@@ -0,0 +1,25 @@
+let dia = parseInt(prompt("ingrese un numero de 1-7"));
+if (dia == 1){
+    console.log("es domingo")
+} else if (dia == 2){
+    console.log("es Lunes")
+} else if (dia == 3){
+    console.log("es Martes ")
+} else if (dia == 4 ){
+    console.log("es Miercoles")
+} else if (dia == 5){
+    console.log("es Jueves")
+} else if (dia == 6){
+    console.log("es Viernes")
+} else if (dia == 7){
+    console.log("es Sabado")
+} else {
+    console.log("el numero es invalido")
+} 
+
+
+
+
+
+
+
